528b074fb53cf1ac2669b1f4ed3023b3433e98b2
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-01-16 18:41:21 +0100

    Changes name of GroupStream to GroupsStream

--- tap_zammad/tap.py
+++ tap_zammad/tap.py
@@ -5,9 +5,9 @@ from typing import List
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
 
-from tap_zammad.streams import GroupStream, TagsStream, TicketsStream, UsersStream
+from tap_zammad.streams import GroupsStream, TagsStream, TicketsStream, UsersStream
 
-STREAM_TYPES = [TicketsStream, TagsStream, UsersStream, GroupStream]
+STREAM_TYPES = [TicketsStream, TagsStream, UsersStream, GroupsStream]
 
 
 class TapZammad(Tap):
